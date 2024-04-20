5023cd338632f68739edb4845ba13a259f62adc2
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-04-20 20:39:47 +0300

    Delete Euler 2 directory

--- Euler 2/Functions.h
+++
@@ -1,6 +0,0 @@
-/**
- * @brief : Считает сумм чётных чисел фибоначчи, меньших заданного
- * @param upper_bound_ : Верхняя граница
- * @return : Сумма чётных чисел фибоначчи, меньших заданного
-*/
-unsigned long long EvenFibSum(unsigned long long upper_bound_);
