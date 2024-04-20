e0a7754b3bcd2dc94adeee94d9d1c3f4d7f454c6
arkhamknight0878 <147205305+arkhamknight0878@users.noreply.github.com>
2024-04-20 20:22:13 +0300

    Add files via upload

--- Euler 2/Functions.h
+++ Euler 2/Functions.h
@@ -1,6 +1,6 @@
 /**
  * @brief : Считает сумму чётных чисел фибоначи, меньших заданного
  * @param upper_bound_ : Верхняя граница
- * @return : Суммуа чётных чисел фибоначи, меньших заданного
+ * @return : Сумму чётных чисел фибоначи, меньших заданного
 */
 unsigned long long EvenFibSum(unsigned long long upper_bound_);
